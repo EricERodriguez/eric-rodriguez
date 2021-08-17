62ac13b0a6fe613e32082db113f19c711c893209
EricERodriguez <eric92rodriguez@gmail.com>
2021-08-17 10:52:56 -0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IncrementarDecrementarComponent } from './components/incrementar-decrementar/incrementar-decrementar.component';
+import { MultiplicarDividirComponent } from './components/multiplicar-dividir/multiplicar-dividir.component';
+import { ResetearComponent } from './components/resetear/resetear.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncrementarDecrementarComponent,
+    MultiplicarDividirComponent,
+    ResetearComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/multiplicar-dividir/multiplicar-dividir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multiplicar-dividir',
+  templateUrl: './multiplicar-dividir.component.html',
+  styleUrls: ['./multiplicar-dividir.component.css']
+})
+export class MultiplicarDividirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
